418b0996051964252eef267bb1722b16ca4f4da7
Matthew Bell <bellmj@g.cofc.edu>
2016-10-10 21:52:41 -0400

    Game now Generates Board and randomaly spaws enemys and food no turn based logic yet

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -2,11 +2,26 @@
 using System.Collections;
 
 public class GameManager : MonoBehaviour {
+    public static GameManager instance = null;
+    public BoardManager boardScript;
 
-	// Use this for initialization
-	void Start () {
-	
+    private int level = 3;
+
+    // Use this for initialization
+    void Awake() {
+        if (instance == null) {
+            instance = this;
+        } else if ( instance != this) {
+            Destroy(gameObject);
+        }
+
+        DontDestroyOnLoad(gameObject);
+        boardScript = GetComponent<BoardManager>();
+        InitGame();
 	}
+    void InitGame() {
+        boardScript.SetupScene(level);
+    }
 	
 	// Update is called once per frame
 	void Update () {
